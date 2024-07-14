2c70ff7eefb6ece343bb1cbbb296d94d3ed8bb71
Pieter Vander Vennet <pietervdvn@posteo.net>
2024-07-14 12:03:55 +0200

    Add assets

--- README.md
+++
@@ -1,4 +0,0 @@
-# mapcomplete-velopark
-SIngle-app deployment of MapComplete/velopark
-
-Please see [https://github.com/pietervdvn/MapComplete/](https://github.com/pietervdvn/MapComplete/) for the actual repo, issues, PR's, ...
---
+++ assets/land-18c02d43.js
@@ -0,0 +1,2 @@
+import{O as i}from"./OsmConnection-b873e866.js";console.log("Authorizing...");new i().finishLogin(o=>{const n=window.location.protocol+"//"+window.location.host+"/index.html";o??(o=n),o.indexOf("/land")>0&&(o=n),console.log("Redirecting to",o),window.location.href=o});
+//# sourceMappingURL=land-18c02d43.js.map
